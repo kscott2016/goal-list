cd263cb06b3186d713ef24ff189e4c783502e0b9
kscott2016 <krystinascott2008@yahoo.com>
2024-03-05 23:24:27 -0600

    add user auh to cbv and functions

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
   path('about/', views.about, name='about'),
   path('goals/', views.goal_index, name='goal-index'),
   path('goals/<int:goal_id>/', views.goal_detail, name='goal-detail'),
-   path('goals/create/', views.GoalCreate.as_view(), name='goal-create'),
-   path('goals/<int:pk>/update/', views.GoalUpdate.as_view(), name='goal-update'),
+  path('goals/create/', views.GoalCreate.as_view(), name='goal-create'),
+  path('goals/<int:pk>/update/', views.GoalUpdate.as_view(), name='goal-update'),
   path('goals/<int:pk>/delete/', views.GoalDelete.as_view(), name='goal-delete'),
+  path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Goal 
 
 # class Goal:
@@ -15,26 +19,51 @@ from .models import Goal
 def about(request):
   return render(request, 'about.html')
 
+@login_required
 def goal_index(request):
-  goals= Goal.objects.all()
+  goals = Goal.objects.filter(user=request.user)
   return render(request, 'goals/index.html', { 'goals': goals })
 
+@login_required
 def goal_detail(request, goal_id):
   goal = Goal.objects.get(id=goal_id)
   return render(request, 'goals/detail.html', { 'goal': goal })
 
-class GoalCreate(CreateView):
+
+class GoalCreate(LoginRequiredMixin, CreateView):
   model = Goal
   fields = '__all__'
+
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
   success_url='/goals/'
 
-class GoalUpdate(UpdateView):
+class GoalUpdate(LoginRequiredMixin, UpdateView):
   model = Goal
   fields = ['name', 'description', 'category','priority']
 
-class GoalDelete(DeleteView):
+class GoalDelete(LoginRequiredMixin, DeleteView):
   model = Goal
   success_url = '/goals/'
 
 class Home(LoginView):
   template_name = 'home.html'
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in
+      login(request, user)
+      return redirect('cat-index')
+    else:
+      error_message = 'Invalid sign up - try again'
+
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
+  
