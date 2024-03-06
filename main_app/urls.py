3f63c92866220e0162e08a2bad08024fbbd8ef26
kscott2016 <krystinascott2008@yahoo.com>
2024-03-05 22:44:58 -0600

    delete and update functionality added

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
-  # route for cats index
   path('goals/', views.goal_index, name='goal-index'),
+  path('goals/<int:goal_id>/', views.goal_detail, name='goal-detail'),
+   path('goals/create/', views.GoalCreate.as_view(), name='goal-create'),
+   path('goals/<int:pk>/update/', views.GoalUpdate.as_view(), name='goal-update'),
+  path('goals/<int:pk>/delete/', views.GoalDelete.as_view(), name='goal-delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Goal 
 
-class Goal:
-  def __init__(self, name, description, category):
-    self.name = name
-    self.description = description
-    self.category = category
-
+# class Goal:
+#   def __init__(self, name, description, category):
+#     self.name = name
+#     self.description = description
+#     self.category = category
 
 def home(request):
   return render(request, 'home.html')
@@ -13,6 +14,23 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
-# Add new view
 def goal_index(request):
+  goals= Goal.objects.all()
   return render(request, 'goals/index.html', { 'goals': goals })
+
+def goal_detail(request, goal_id):
+  goal = Goal.objects.get(id=goal_id)
+  return render(request, 'goals/detail.html', { 'goal': goal })
+
+class GoalCreate(CreateView):
+  model = Goal
+  fields = '__all__'
+  success_url='/goals/'
+
+class GoalUpdate(UpdateView):
+  model = Goal
+  fields = ['name', 'description', 'category','priority']
+
+class GoalDelete(DeleteView):
+  model = Goal
+  success_url = '/goals/'
